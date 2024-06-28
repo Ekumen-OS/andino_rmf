e1b7eab7a899bc2191cd4248132111d777683eb5
ptientho <ptientho@stevens.edu>
2024-06-28 13:41:14 -0400

    Revert "Modifies fleet adapter to use the template from main branch"
    This reverts commit eac577296c761d83512682f8a606b4a61d223260.

--- andino_fleet_adapter/launch/andino_fleet_adapter.launch.py
+++ andino_fleet_adapter/launch/andino_fleet_adapter.launch.py
@@ -10,12 +10,23 @@ def generate_launch_description():
 
     rmf_config_file = os.path.join(get_package_share_directory('andino_fleet_adapter'), 'config.yaml')
     nav_graph_file = os.path.join(get_package_share_directory('rmf_maps'), 'maps', 'andino_office', 'nav_graphs', '0.yaml')
+
+    '''
+    fleet_adapter = ExecuteProcess(
+        cmd=[[
+            'ros2 run andino_fleet_adapter fleet_adapter ',
+            '-c ', rmf_config_file, ' ', '-n ', nav_graph_file 
+            # also pass config_file and nav_graph
+        ]],
+        shell=True
+    )
+    '''
     
     fleet_adapter = Node(
         package= 'andino_fleet_adapter',
         executable= 'fleet_adapter',
         name= 'andino_fleet_adapter',
-        arguments=['-c', rmf_config_file, '-n', nav_graph_file],#, '-s', ''],
+        arguments=['-c', rmf_config_file, '-n', nav_graph_file],
         output='screen',
     )
     

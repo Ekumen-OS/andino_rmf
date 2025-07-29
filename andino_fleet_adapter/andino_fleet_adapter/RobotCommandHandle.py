73eb24d3926a68b09612811aaf4bc5a9af17720b
camistolo <camistolo@gmail.com>
2025-07-29 17:10:41 -0300

    Doing some more testing
    Signed-off-by: camistolo <camistolo@gmail.com>

--- andino_fleet_adapter/andino_fleet_adapter/RobotCommandHandle.py
+++ andino_fleet_adapter/andino_fleet_adapter/RobotCommandHandle.py
@@ -223,6 +223,9 @@ class RobotCommandHandle(adpt.RobotCommandHandle):
                                 self.state = RobotState.IDLE
                             else:
                                 if self.path_index is not None:
+                                    # self.node.get_logger().info(
+                                    #     f"{self.name} Waiting for "
+                                    #     f"{(waypoint_wait_time - time_now).seconds}s")
                                     self.next_arrival_estimator(
                                         self.path_index, timedelta(seconds=0.0))
 

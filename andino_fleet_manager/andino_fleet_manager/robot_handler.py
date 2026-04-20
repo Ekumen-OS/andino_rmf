2bc1d36efdb256bc463f6d3f10eba54576bccb81
camistolo <40144517+camistolo@users.noreply.github.com>
2026-04-20 13:35:11 -0300

    Set initial pose and add the send_goal and cancel_goal methods (#35)

--- andino_fleet_manager/andino_fleet_manager/robot_handler.py
+++ andino_fleet_manager/andino_fleet_manager/robot_handler.py
@@ -7,104 +7,211 @@
     sending navigation goals and canceling active goals for the robot.
 '''
 
+from enum import Enum
 import threading
 
+from rclpy.action import ActionClient
+from rclpy.action.client import ClientGoalHandle
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 from rclpy.node import Node
-from geometry_msgs.msg import PoseWithCovarianceStamped
+from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
+from rclpy.task import Future
+
+from action_msgs.msg import GoalStatus
+from geometry_msgs.msg import Quaternion, PoseWithCovarianceStamped, PoseStamped
+from nav2_msgs.action import NavigateToPose
 from tf_transformations import quaternion_from_euler
 
 
+class ReturnFlag(Enum):
+    SUCCESS = 0
+    ROBOT_OFFLINE = 1
+    UNINITIALIZED_GOAL_HANDLE = 2
+    NO_SUBSCRIBERS = 3
+
+
 class RobotHandler:
-    """
-    Handles individual robot instances within the fleet manager.
+    VAR_XY = 0.25
+    VAR_YAW = 0.0685
+    
+    IDX_X = 0
+    IDX_Y = 7
+    IDX_YAW = 35
 
-    This class manages the state and communication for a single robot,
-    including pose tracking, initial pose setting, navigation goal management,
-    and subscribing to pose updates from the robot's AMCL localization system.
-    It will also handle sending navigation goals to robots and canceling active
-    goals when requested by the fleet manager.
-    """
-
-    def __init__(self, node: Node, robot_name: str, initial_pose: dict(), lock: threading.Lock):
-        """
-        Initialize a RobotHandler for managing a single robot.
-
-        Args:
-            node (Node): The ROS2 node instance to use for communication
-            robot_name (str): Unique identifier for the robot
-            initial_pose (dict): Dictionary containing robot's initial pose with keys:
-                                'x', 'y', 'z' for position and 'yaw' for orientation
-            lock (threading.Lock): Thread lock for safe concurrent access to robot data
-        """
+    def __init__(
+        self,
+        node: Node,
+        robot_name: str,
+        initial_pose: dict(),
+        lock: threading.Lock,
+        goal_callback_group: MutuallyExclusiveCallbackGroup,
+        pose_callback_group: MutuallyExclusiveCallbackGroup,
+    ):
         self.node = node
         self.robot_name = robot_name
-        self.current_pose: PoseWithCovarianceStamped = None
         self._lock = lock
 
-        self._set_initial_pose(robot_name, initial_pose)
+        self.initial_pose = initial_pose
+        self._goal_handle: ClientGoalHandle = None
+        self.current_pose: PoseWithCovarianceStamped = None
+        self._reset_navigation_data()
 
-        topic_name = "/" + robot_name + "/amcl_pose"
+        self.initialize_publishers_and_subscribers(pose_callback_group)
+
+        self.initialize_actions(goal_callback_group)
+
+        self.current_pose = self._get_initial_pose_msg(self.initial_pose)
+
+        self.node.get_logger().info(f"RobotHandler initialized for robot {robot_name}")
+
+    def initialize_publishers_and_subscribers(self, callback_group: MutuallyExclusiveCallbackGroup):
+        # Publisher for the initial pose
+        qos_profile = QoSProfile(
+            reliability=ReliabilityPolicy.RELIABLE,
+            durability=DurabilityPolicy.TRANSIENT_LOCAL,
+            history=HistoryPolicy.KEEP_LAST,
+            depth=1
+        )
+        initial_pose_topic = "/" + self.robot_name + "/initialpose"
+        self.initial_pose_publisher = self.node.create_publisher(
+            PoseWithCovarianceStamped, initial_pose_topic, qos_profile)
+
+        # Subscriber to the robot's pose updates
+        topic_name = "/" + self.robot_name + "/amcl_pose"
         self.pose_subscriber = self.node.create_subscription(
             PoseWithCovarianceStamped,
             topic_name,
             self._pose_callback,
             10,
+            callback_group=callback_group,
         )
 
-        self.node.get_logger().info(
-            f"[{robot_name}] RobotHandler initialized with pose: [{initial_pose['x']}, {initial_pose['y']}, {initial_pose['z']}]"
+    def initialize_actions(self, callback_group: MutuallyExclusiveCallbackGroup):
+        # Action client for sending navigation goals to the robot
+        action_name = "/" + self.robot_name + "/navigate_to_pose"
+        self._controller_client = ActionClient(
+            self.node, NavigateToPose, action_name, callback_group=callback_group
         )
 
-    def _set_initial_pose(self, robot_name : str, initial_pose: list()):
-        """
-        Set the initial pose of the robot and publish it to the robot's initialpose topic.
+    def publish_initial_pose(self) -> ReturnFlag:
+        if self.initial_pose_publisher.get_subscription_count() == 0:
+            self.node.get_logger().debug(f"Initial pose could not be published for robot {self.robot_name} because there are no subscribers for the topic")
+            return ReturnFlag.NO_SUBSCRIBERS
+        initial_pose_msg = self._get_initial_pose_msg(self.initial_pose)
+        self.node.get_logger().info(f"Publishing initial pose for {self.robot_name}")
+        self.initial_pose_publisher.publish(initial_pose_msg)
+        return ReturnFlag.SUCCESS
 
-        This method creates and publishes a PoseWithCovarianceStamped message containing
-        the robot's initial position and orientation to help with localization.
-
-        Args:
-            robot_name (str): The name of the robot
-            initial_pose (dict): Dictionary containing initial pose data with keys:
-                               'x', 'y', 'z' for position and 'yaw' for orientation
-        """
-        topic_name = "/" + robot_name + "/initialpose"
-        initial_pose_publisher = self.node.create_publisher(PoseWithCovarianceStamped, topic_name, 10)
+    def _get_initial_pose_msg(self, initial_pose: dict) -> PoseWithCovarianceStamped:
         initial_pose_msg = PoseWithCovarianceStamped()
         initial_pose_msg.header.frame_id = 'map'
+        initial_pose_msg.header.stamp = self.node.get_clock().now().to_msg()
 
+        # Set position from the configuration dictionary, not hardcoded values.
         initial_pose_msg.pose.pose.position.x = initial_pose["x"]
         initial_pose_msg.pose.pose.position.y = initial_pose["y"]
         initial_pose_msg.pose.pose.position.z = initial_pose["z"]
-        orientation = quaternion_from_euler(0, 0, initial_pose["yaw"])
 
+        # Calculate and set orientation from the yaw value in the configuration.
+        orientation = quaternion_from_euler(0.0, 0.0, initial_pose["yaw"])
         initial_pose_msg.pose.pose.orientation.x = orientation[0]
         initial_pose_msg.pose.pose.orientation.y = orientation[1]
         initial_pose_msg.pose.pose.orientation.z = orientation[2]
         initial_pose_msg.pose.pose.orientation.w = orientation[3]
 
-        initial_pose_publisher.publish(initial_pose_msg)
-        self.node.get_logger().info(
-            f"Initial pose set for robot {robot_name}: "
-            f"[{initial_pose_msg.pose.pose.position.x}, "
-            f"{initial_pose_msg.pose.pose.position.y}, "
-            f"{initial_pose_msg.pose.pose.orientation.z}]"
-        )
+        # x and y are uncertain within a 0.5m radius and its z, roll, pitch and yaw values are completely unknown
+        # The covariance matrix is a 6x6 matrix stored as a 36-element array.
+        # The diagonal elements correspond to variance in x, y, z, roll, pitch, yaw.
+        # Covariance[0] is variance for x, [7] is for y, [14] for z, etc.
+        initial_pose_msg.pose.covariance[self.IDX_X] = self.VAR_XY
+        initial_pose_msg.pose.covariance[self.IDX_Y] = self.VAR_XY
+        initial_pose_msg.pose.covariance[self.IDX_YAW] = self.VAR_YAW
+        return initial_pose_msg
 
-        self.current_pose = initial_pose_msg
+    def is_robot_online(self) -> ReturnFlag:
+        if self._controller_client.wait_for_server(timeout_sec=1.0):
+            return ReturnFlag.SUCCESS
+        return ReturnFlag.ROBOT_OFFLINE
+
 
     def _pose_callback(self, msg: PoseWithCovarianceStamped):
-        """
-        Callback function for processing incoming pose updates from the robot.
-
-        This method is called whenever a new pose message is received from the
-        robot's AMCL localization system. It updates the current pose with
-        thread-safe access using the provided lock.
-
-        Args:
-            msg (PoseWithCovarianceStamped): The pose message received from the robot
-        """
         with self._lock:
-            self.node.get_logger().info(
-                f"[{self.robot_name}] Current pose [{msg.pose.pose.position.x}, {msg.pose.pose.position.y}, {msg.pose.pose.orientation.z}]"
-            )
             self.current_pose = msg
+
+    def send_goal(self, goal: list()) -> ReturnFlag:
+        if not self.is_robot_online():
+            return ReturnFlag.ROBOT_OFFLINE
+
+        self._reset_navigation_data()
+
+        goal_msg = NavigateToPose.Goal()
+        goal_msg.pose.header.frame_id = "map"
+        goal_msg.pose.pose.position.x = goal[0]
+        goal_msg.pose.pose.position.y = goal[1]
+
+        quaternion = quaternion_from_euler(0, 0, goal[2])
+        orientation = Quaternion()
+        orientation.x = quaternion[0]
+        orientation.y = quaternion[1]
+        orientation.z = quaternion[2]
+        orientation.w = quaternion[3]
+        goal_msg.pose.pose.orientation = orientation
+
+        send_goal_future = self._controller_client.send_goal_async(goal_msg, self._feedback_callback)
+        send_goal_future.add_done_callback(self._goal_response_callback)
+        return ReturnFlag.SUCCESS
+
+    def _feedback_callback(self, feedback_msg : PoseStamped):
+        feedback = feedback_msg.feedback
+        self.current_pose.header = feedback.current_pose.header
+        self.current_pose.pose.pose = feedback.current_pose.pose
+        self._navigation_time = feedback.navigation_time
+        self._estimated_time_remaining = feedback.estimated_time_remaining
+        self._number_of_recoveries = feedback.number_of_recoveries
+        self._distance_remaining = feedback.distance_remaining
+
+    def _goal_response_callback(self, future: Future):
+        self._goal_handle = future.result()
+        if not self._goal_handle.accepted:
+            self.node.get_logger().info(f"Goal rejected")
+            return
+
+        self.node.get_logger().info("Goal accepted")
+
+        self._get_result_future = self._goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self._get_result_callback)
+
+    def _get_result_callback(self, future : Future):
+        if self._goal_handle.status == GoalStatus.STATUS_CANCELED:
+            self._navigation_completed = False
+            return
+        self._distance_remaining = 0.0
+        self._navigation_completed = True
+        self.node.get_logger().info(f"Goal completed")
+
+
+    def cancel_goal(self) -> ReturnFlag:
+       if not self.is_robot_online():
+            return ReturnFlag.ROBOT_OFFLINE
+       if self._goal_handle is None:
+            return ReturnFlag.UNINITIALIZED_GOAL_HANDLE
+       future = self._goal_handle.cancel_goal_async()
+       future.add_done_callback(self._cancel_response_callback)
+       self._reset_navigation_data()
+       return ReturnFlag.SUCCESS
+
+    def _cancel_response_callback(self, future: Future):
+       self._reset_navigation_data()
+
+    def _reset_navigation_data(self):
+        self._navigation_completed = False
+        self._navigation_time = 0.0
+        self._estimated_time_remaining = 0.0
+        self._number_of_recoveries = 0
+        self._distance_remaining = 0.0
+
+    def get_navigation_completed(self) -> bool:
+        return self._navigation_completed
+    
+    def get_distance_remaining(self):
+        return self._distance_remaining
